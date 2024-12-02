de4ef94886d5d127e1484d0436ec2aae84e13500
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2024-12-02 09:54:29 +0900

    バトル画面の描画を一般化

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import PyxelUniversalFont as puf
 WIDTH, HEIGHT = 640, 400
 SCENE_BATTLE = 0
 TRANSPARENT_COLOR = 2
-SCALE = 4.0
+MESSAGE_POSITION_X = 40
+FISRT_MESSAGE_POSITION_Y = HEIGHT * 0.66 + 10
+FISRT_MESSAGE_POSITION_Y = HEIGHT * 0.66 + 10
+SECOND_MESSAGE_POSITION_Y = HEIGHT * 0.66 + 60
+MESSAGE_FONT_SIZE = 24
 
 TYPES = [None, "ノーマル", "ほのお", "みず", "くさ", "ひこう", "ドラゴン", "どく"]
 WRITER = puf.Writer("misaki_gothic.ttf")
@@ -13,8 +17,6 @@ WRITER = puf.Writer("misaki_gothic.ttf")
 class Monster:
     def __init__(
         self,
-        x,
-        y,
         u,
         v,
         w,
@@ -29,8 +31,6 @@ class Monster:
         sp_difense,
         speed,
     ):
-        self.x = x
-        self.y = y
         self.u = u
         self.v = v
         self.w = w
@@ -45,24 +45,38 @@ class Monster:
         self.sp_difense = sp_difense
         self.speed = speed
 
-    def draw(self):
+    def draw_monster(self, x, y, scale, is_facing_right=False):
         # モンスターを描画
-        pyxel.blt(
-            self.x,
-            self.y,
-            0,
-            self.u,
-            self.v,
-            self.w,
-            self.h,
-            TRANSPARENT_COLOR,
-            scale=SCALE,
-        )
+        if is_facing_right:
+            pyxel.blt(
+                x,
+                y,
+                0,
+                self.u,
+                self.v,
+                self.w * (-1),
+                self.h,
+                TRANSPARENT_COLOR,
+                scale=scale,
+            )
+        else:
+            pyxel.blt(
+                x,
+                y,
+                0,
+                self.u,
+                self.v,
+                self.w,
+                self.h,
+                TRANSPARENT_COLOR,
+                scale=scale,
+            )
+
+    def draw_name_and_hp(self, x, y, is_visible=True):
         # 名前を描画
-        WRITER.draw(self.x - 40, self.y - 100, self.name, 16, 0)
+        WRITER.draw(x, y, self.name, 16, 0)
         # HPバーを描画
-        pyxel.rect(self.x - 40, self.y - 80, 100, 10, 0)
-        hp_color = 11
+        pyxel.rect(x, y + 20, 100, 10, 0)
         hp_ratio = self.hp_now / self.hp
         if hp_ratio < 0.25:
             # HPが1/4未満のとき赤色
@@ -70,11 +84,31 @@ class Monster:
         elif hp_ratio < 0.5:
             # HPが1/4以上1/2未満のとき黄色
             hp_color = 10
-        pyxel.rect(self.x - 40, self.y - 80, 100 * hp_ratio, 10, hp_color)
-        # HP情報を描画
-        WRITER.draw(
-            self.x - 40, self.y - 60, f"{str(self.hp_now)}/{str(self.hp)}", 16, 0
-        )
+        else:
+            hp_color = 11
+        pyxel.rect(x, y + 20, 100 * hp_ratio, 10, hp_color)
+        if is_visible:
+            # HP情報を描画
+            WRITER.draw(x, y + 35, f"{str(self.hp_now)}/{str(self.hp)}", 16, 0)
+
+
+ALL_MONSTERS = [
+    Monster(
+        0,
+        17,
+        20,
+        17,
+        "フシギダネ",
+        "くさ",
+        "どく",
+        120,
+        69,
+        69,
+        85,
+        85,
+        65,
+    )
+]
 
 
 class App:
@@ -82,41 +116,11 @@ class App:
         pyxel.init(WIDTH, HEIGHT)
         pyxel.load("./resource.pyxres")
         self.scene = SCENE_BATTLE
+        self.my_monsters = []
+        self.opponenent_monsters = []
 
-        self.monster1 = Monster(
-            WIDTH * 0.66,
-            HEIGHT * 0.33,
-            0,
-            17,
-            20,
-            17,
-            "フシギダネ",
-            "くさ",
-            "どく",
-            120,
-            69,
-            69,
-            85,
-            85,
-            65,
-        )
-        self.monster2 = Monster(
-            WIDTH * 0.33,
-            HEIGHT * 0.33,
-            0,
-            17,
-            -20,
-            17,
-            "フシギダネ",
-            "くさ",
-            "どく",
-            120,
-            69,
-            69,
-            85,
-            85,
-            65,
-        )
+        self.my_monsters.append(ALL_MONSTERS[0])
+        self.opponenent_monsters.append(ALL_MONSTERS[0])
 
         pyxel.run(self.update, self.draw)
 
@@ -125,17 +129,48 @@ class App:
 
     def draw(self):
         pyxel.cls(7)
-        self.monster1.draw()
-        self.monster2.draw()
-        # 「YOURS」を描画
-        WRITER.draw(WIDTH * 0.33 - 30, HEIGHT * 0.33 + 60, "YOURS", 32, 8)
-        # メッセージ表示枠を描画
-        pyxel.rectb(15, HEIGHT * 0.66 - 15, WIDTH - 30, HEIGHT * 0.33, 0)
-        # メッセージを描画
-        WRITER.draw(40, HEIGHT * 0.66 + 10, "どうする？", 24, 0)
-        WRITER.draw(80, HEIGHT * 0.66 + 60, "戦う", 24, 0)
-        WRITER.draw(200, HEIGHT * 0.66 + 60, "モンスター", 24, 0)
-        WRITER.draw(400, HEIGHT * 0.66 + 60, "降参する", 24, 0)
+        if self.scene == SCENE_BATTLE:
+            # 「YOURS」を描画
+            WRITER.draw(WIDTH * 0.33 - 30, HEIGHT * 0.33 + 60, "YOURS", 32, 8)
+            # メッセージ表示枠を描画
+            pyxel.rectb(15, HEIGHT * 0.66 - 15, WIDTH - 30, HEIGHT * 0.33, 0)
+            # モンスターを描画
+            self.my_monsters[0].draw_monster(WIDTH * 0.33, HEIGHT * 0.33, 4, True)
+            self.opponenent_monsters[0].draw_monster(WIDTH * 0.66, HEIGHT * 0.33, 4)
+            # モンスターの名前とHPを描画
+            self.my_monsters[0].draw_name_and_hp(WIDTH * 0.33 - 40, HEIGHT * 0.33 - 100)
+            self.opponenent_monsters[0].draw_name_and_hp(
+                WIDTH * 0.66 - 40, HEIGHT * 0.33 - 100, False
+            )
+            # メッセージを描画
+            WRITER.draw(
+                MESSAGE_POSITION_X,
+                FISRT_MESSAGE_POSITION_Y,
+                "どうする？",
+                MESSAGE_FONT_SIZE,
+                0,
+            )
+            WRITER.draw(
+                MESSAGE_POSITION_X + 40,
+                SECOND_MESSAGE_POSITION_Y,
+                "戦う",
+                MESSAGE_FONT_SIZE,
+                0,
+            )
+            WRITER.draw(
+                MESSAGE_POSITION_X + 160,
+                SECOND_MESSAGE_POSITION_Y,
+                "モンスター",
+                MESSAGE_FONT_SIZE,
+                0,
+            )
+            WRITER.draw(
+                MESSAGE_POSITION_X + 360,
+                SECOND_MESSAGE_POSITION_Y,
+                "降参する",
+                MESSAGE_FONT_SIZE,
+                0,
+            )
 
 
 App()
